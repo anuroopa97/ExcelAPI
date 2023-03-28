fca872c8169a4927cfc9b22ec1590c9d88449b0d
anuroopa97 <anuroopa.palla5@gmail.com>
2023-03-27 21:38:40 -0400

    all files commit

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['excelapi_0',['ExcelAPI',['../namespace_excel_a_p_i.html',1,'']]]
+  ['excelapi_0',['ExcelAPI',['../namespace_excel_a_p_i.html',1,'ExcelAPI'],['../md__r_e_a_d_m_e.html',1,'(Global Namespace)']]]
 ];
--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -2,20 +2,23 @@ var indexSectionsWithContent =
 {
   0: "esw",
   1: "sw",
-  2: "e"
+  2: "e",
+  3: "e"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "namespaces"
+  2: "namespaces",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Namespaces"
+  2: "Namespaces",
+  3: "Pages"
 };
 
